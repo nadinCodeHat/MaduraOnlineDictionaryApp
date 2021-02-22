ee8b0509ee89cb10acfacb21c443ad52be038090
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2021-02-22 23:03:40 +0530

    Modified

--- app/src/main/java/com/example/maduraonlinedictionaryapp/MainActivity.java
+++ app/src/main/java/com/example/maduraonlinedictionaryapp/MainActivity.java
@@ -1,14 +1,111 @@
 package com.example.maduraonlinedictionaryapp;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String DEBUG_TAG = "HttpExample";
+    private EditText key_word;
+    private TextView textView;
+    private Button translateBTN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        key_word = findViewById(R.id.keyword);
+        textView = findViewById(R.id.textDisplay);
+        translateBTN = (Button) findViewById(R.id.translateBtn);
+        translateBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stringUrl = "https://www.maduraonline.com/?find=" + key_word.getText().toString();
+                ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+                if (networkInfo != null && networkInfo.isConnected()) {
+                    new DownloadWebpageTask().execute(stringUrl);
+                } else {
+                    textView.setText("No network connection available.");
+                }
+            }
+        });
+    }
+
+    private class DownloadWebpageTask extends AsyncTask<String,Void,String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            ArrayList<String> words = new ArrayList<>();
+            Pattern pattern = Pattern.compile("<td class=\"td\">(.*?)</td>");
+            Matcher matcher = pattern.matcher(result);
+            while (matcher.find()) {
+                words.add(matcher.group(1));
+                for (int i=1; i<words.size()-1; i++) {
+                    textView.setText(textView.getText() + "\n" + matcher.group(1));
+                }
+            }
+        }
+
+        private String downloadUrl(String myurl) throws IOException {
+            InputStream is = null;
+            int len = 500;
+            try {
+                URL url = new URL(myurl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                //starts the query
+                conn.connect();
+                int response = conn.getResponseCode();
+                Log.d(DEBUG_TAG, "This response is: " + response);
+                is = conn.getInputStream();
+
+                String contentAsString = readIt(is, len);
+                return contentAsString;
+            } finally {
+                if (is != null) {
+                    is.close();
+                }
+            }
+        }
+
+        public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+            Reader reader = null;
+            reader = new InputStreamReader(stream, "UTF-8");
+            char[] buffer = new char[len];
+            reader.read(buffer);
+            return new String(buffer);
+        }
     }
 }
