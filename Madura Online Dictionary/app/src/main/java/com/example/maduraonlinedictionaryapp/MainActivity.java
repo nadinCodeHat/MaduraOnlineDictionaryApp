06f20beac917bae0fdd90ee79fcceff4e4c52320
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2021-02-27 22:37:26 +0530

    Modified

--- Madura Online Dictionary/app/src/main/java/com/example/maduraonlinedictionaryapp/MainActivity.java
+++ Madura Online Dictionary/app/src/main/java/com/example/maduraonlinedictionaryapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.maduraonlinedictionaryapp;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -10,8 +12,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,33 +26,33 @@ import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
     private static final String DEBUG_TAG = "HttpExample";
-    private EditText key_word;
-    private TextView textView;
-    private Button translateBTN;
+    private EditText keyword_txt;
+    private TextView textView_display;
+    private Button translate_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        key_word = (EditText)findViewById(R.id.keyword);
-        textView = (TextView)findViewById(R.id.textDisplay);
-        translateBTN = (Button) findViewById(R.id.translateBtn);
-        translateBTN.setOnClickListener(new View.OnClickListener() {
+        keyword_txt = (EditText) findViewById(R.id.keyword);
+        textView_display = (TextView) findViewById(R.id.textDisplay);
+        translate_btn = (Button) findViewById(R.id.translateBtn);
+        translate_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String stringUrl = "https://www.maduraonline.com/?find=" + key_word.getText().toString();
+                String stringUrl = "https://www.maduraonline.com/?find=" + keyword_txt.getText().toString();
                 ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
                 NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
                 if (networkInfo != null && networkInfo.isConnected()) {
                     new DownloadWebpageTask().execute(stringUrl);
                 } else {
-                    textView.setText("No network connection available.");
+                    textView_display.setText("No network connection available.");
                 }
             }
         });
     }
 
-    private class DownloadWebpageTask extends AsyncTask<String,Void,String> {
-
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String... urls) {
             try {
@@ -70,32 +70,7 @@ public class MainActivity extends AppCompatActivity {
             while (matcher.find()) {
                 words.add(matcher.group(1));
                 for (int i=1; i<words.size()-1; i++) {
-                    textView.setText(textView.getText() + "\n" + matcher.group(1));
-                }
-            }
-        }
-
-        private String downloadUrl(String myurl) throws IOException {
-            InputStream is = null;
-            int len = 500;
-            try {
-                URL url = new URL(myurl);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(10000);
-                conn.setConnectTimeout(15000);
-                conn.setRequestMethod("GET");
-                conn.setDoInput(true);
-                //starts the query
-                conn.connect();
-                int response = conn.getResponseCode();
-                Log.d(DEBUG_TAG, "This response is: " + response);
-                is = conn.getInputStream();
-
-                String contentAsString = readIt(is, len);
-                return contentAsString;
-            } finally {
-                if (is != null) {
-                    is.close();
+                    textView_display.setText(textView_display.getText() + "\n" + matcher.group(1));
                 }
             }
         }
@@ -106,6 +81,29 @@ public class MainActivity extends AppCompatActivity {
             char[] buffer = new char[len];
             reader.read(buffer);
             return new String(buffer);
+        }
+
+        private String downloadUrl(String myUrl) throws IOException {
+            InputStream is = null;
+            int len = 4000;
+            try {
+                URL url = new URL(myUrl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.connect();
+                int response = conn.getResponseCode();
+                Log.d(DEBUG_TAG, "The response is: " + response);
+                is = conn.getInputStream();
+                String contentAsString = readIt(is, len);
+                return contentAsString;
+            } finally {
+                if (is != null) {
+                    is.close();
+                }
+            }
         }
     }
 }
